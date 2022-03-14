cd4f2c7c8cea25462f354fc0d859b14168355fc8
TheSwissWizard <55490152+TheSwissWizard@users.noreply.github.com>
2022-03-14 20:35:17 +0100

    Update deployToNexus.groovy

--- vars/deployToNexus.groovy
+++ vars/deployToNexus.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(Map config [:]) {
+def call(Map config = [:]) {
     // Read POM xml file using 'readMavenPom' step , this step 'readMavenPom' is included in: https://plugins.jenkins.io/pipeline-utility-steps
     pom = readMavenPom file: "pom.xml";
 
