ceb5181f14a6b782f2401d805b3e82dfc111dec8
TheSwissWizard <55490152+TheSwissWizard@users.noreply.github.com>
2022-03-14 15:57:49 +0100

    Create constants.groovy

---
+++ src/ch/bs/hug/constants.groovy
@@ -0,0 +1,10 @@
+package ch.bs.hug
+
+class Constants {
+
+  public final static String NEXUS_VERSION = "nexus3";
+  public final static String NEXUS_PROTOCOL = "https";
+  public final static String NEXUS_URL = "nexus.florian-hug.ch";
+  public final static String NEXUS_REPOSITORY = "ch.bs.hug";
+  public final static String NEXUS_CREDENTIAL_ID = "nexus-credentials";
+}
