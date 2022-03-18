6e637ab032a17d4ce36e44bf999bfc37da57f77d
TheSwissWizard <55490152+TheSwissWizard@users.noreply.github.com>
2022-03-18 16:34:50 +0100

    Update deployToStage.groovy

--- vars/deployToStage.groovy
+++ vars/deployToStage.groovy
@@ -2,10 +2,18 @@
 
 def call(Map config = [:]) {
   def pom = readMavenPom file: "pom.xml"
+  def fullImageName = config.imageName + "${pom.version}"
+  
+  def dockerStopCommand = "docker stop " + config.imageName
+  def dockerRemoveImageCommand = "docker rmi $(docker images -q ${env.DOCKER_BASE_URL)/${fullImageName})"
+  def dockerPullCommand = "docker pull ${env.DOCKER_BASE_URL}/${fullImageName}"
+  
   withCredentials([sshUserPrivateKey(credentialsId: 'app-ssh', keyFileVariable: 'keyfile', usernameVariable: 'sshuser')]) {
     sh "ssh -i $keyfile -o StrictHostKeyChecking=no $sshuser " +
       "'" +
-      "docker pull ${env.DOCKER_BASE_URL}/" + config.imageName + ":${pom.version}" +
+      "${dockerStopCommand};" +
+      "${dockerRemoveImageCommand};" +
+      "${dockerPullCommand}" +
       "'"
   }
 }
