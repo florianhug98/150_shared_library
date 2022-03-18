7cdb6a7360cbcda32db9ff1d09fb0aa9cc80df03
TheSwissWizard <55490152+TheSwissWizard@users.noreply.github.com>
2022-03-18 13:30:30 +0100

    Update deployToStage.groovy

--- vars/deployToStage.groovy
+++ vars/deployToStage.groovy
@@ -2,7 +2,7 @@
 
 def call() {
   withCredentials([sshUserPrivateKey(credentialsId: 'app-ssh', keyFileVariable: 'keyfile', usernameVariable: 'sshuser')]) {
-    sh "ssh -i $keyfile $sshuser " +
+    sh "ssh -i $keyfile -o StrictHostKeyChecking=no $sshuser " +
       "docker ps"
   }
 }
