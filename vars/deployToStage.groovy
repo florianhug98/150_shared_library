565b6af1d427721547afb9c9d37f7464ed87568a
TheSwissWizard <55490152+TheSwissWizard@users.noreply.github.com>
2022-03-18 17:00:04 +0100

    Update deployToStage.groovy

--- vars/deployToStage.groovy
+++ vars/deployToStage.groovy
@@ -2,13 +2,14 @@
 
 def call(Map config = [:]) {
   def pom = readMavenPom file: "pom.xml"
-  def fullImageName = config.imageName + ":${pom.version}"
+  def imageName = "${pom.artifactId}"
+  def fullImageName = "${imageName}:${pom.version}"
   
-  def dockerStopCommand = "docker stop " + config.imageName
-  def dockerRemoveImageCommand = "docker rmi \$(docker images -q ${env.DOCKER_BASE_URL}/" + config.imageName + ")"
-  def dockerRemoveContainerCommand = "docker rm " + config.imageName
+  def dockerStopCommand = "docker stop ${imageName}"
+  def dockerRemoveImageCommand = "docker rmi \$(docker images -q ${env.DOCKER_BASE_URL}/${imageName})"
+  def dockerRemoveContainerCommand = "docker rm ${imageName}"
   def dockerPullCommand = "docker pull ${env.DOCKER_BASE_URL}/${fullImageName}"
-  def dockerRunCommand = "docker run -d --name " + config.imageName + " ${env.DOCKER_BASE_URL}/${fullImageName}"
+  def dockerRunCommand = "docker run -d --name ${imageName} ${env.DOCKER_BASE_URL}/${fullImageName}"
   
   withCredentials([sshUserPrivateKey(credentialsId: 'app-ssh', keyFileVariable: 'keyfile', usernameVariable: 'sshuser')]) {
     sh "ssh -i $keyfile -o StrictHostKeyChecking=no $sshuser " +
