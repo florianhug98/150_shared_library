683800ba212ecd76f09d28b4c96bc54a42e12d88
TheSwissWizard <55490152+TheSwissWizard@users.noreply.github.com>
2022-03-18 14:33:06 +0100

    Update deployToStage.groovy

--- vars/deployToStage.groovy
+++ vars/deployToStage.groovy
@@ -3,6 +3,8 @@
 def call(Map config = [:]) {
   withCredentials([sshUserPrivateKey(credentialsId: 'app-ssh', keyFileVariable: 'keyfile', usernameVariable: 'sshuser')]) {
     sh "ssh -i $keyfile -o StrictHostKeyChecking=no $sshuser " +
-      "docker pull nexus.florian-hug.ch/repository/docker-repo/spring-jenkins-demo-arm:0.2"
+      "'" +
+      "docker pull nexus.florian-hug.ch/repository/docker-repo/spring-jenkins-demo-arm:0.2" +
+      "'"
   }
 }
