cd6bc28748749937a790fee7fa61bccb465cb943
TheSwissWizard <55490152+TheSwissWizard@users.noreply.github.com>
2022-03-18 16:13:02 +0100

    Update deployToStage.groovy

--- vars/deployToStage.groovy
+++ vars/deployToStage.groovy
@@ -1,6 +1,8 @@
 #!/usr/bin/env groovy
 
 def call(Map config = [:]) {
+  pom = readMavenPom file: "pom.xml"
+  echo "config.imageName:${pom.version"
   withCredentials([sshUserPrivateKey(credentialsId: 'app-ssh', keyFileVariable: 'keyfile', usernameVariable: 'sshuser')]) {
     sh "ssh -i $keyfile -o StrictHostKeyChecking=no $sshuser " +
       "'" +
