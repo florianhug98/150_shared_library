3403fd78688aa499bfc881b4285c014748744bf6
TheSwissWizard <55490152+TheSwissWizard@users.noreply.github.com>
2022-03-18 13:47:00 +0100

    Update deployToStage.groovy

--- vars/deployToStage.groovy
+++ vars/deployToStage.groovy
@@ -4,7 +4,7 @@ def call() {
   withCredentials([sshUserPrivateKey(credentialsId: 'app-ssh', keyFileVariable: 'keyfile', usernameVariable: 'sshuser')]) {
     withCredentials([usernamePassword(credentialsId: 'nexus-credentials', passwordVariable: 'password', usernameVariable: 'username')]) {
       sh "ssh -i $keyfile -o StrictHostKeyChecking=no $sshuser " +
-        "docker login -p $password -u $username ${env.DOCKER_REGISTRY_BASE_URL} " +
+        "docker login -p $password -u $username ${env.NEXUS_URL} " +
         "docker ps"
     }
   }
